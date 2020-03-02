5c3d5550a7ba3c4fe5ca2f6e45aea184e4aae0d7
OmarDawoud <omardawoud@cmail.carleton.ca>
2020-03-02 12:49:20 -0500

    Created Repository classes and added test function
    New Classes:
    +StudentRepository
    +InstructorRepository
    
    Added new Dependency in Pom.xml
    Added test function in BootApplication

--- src/main/java/Application/BootApplication.java
+++ src/main/java/Application/BootApplication.java
@@ -1,12 +1,12 @@
 package Application;
 
-import Model.*;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import Model.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
