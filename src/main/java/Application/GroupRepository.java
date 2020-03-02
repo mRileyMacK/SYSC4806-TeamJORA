2c347cecfe8705d0246023975e90382c2fa6d929
OmarDawoud <omardawoud@cmail.carleton.ca>
2020-03-02 12:58:53 -0500

    Created GroupRepository

---
+++ src/main/java/Application/GroupRepository.java
@@ -0,0 +1,8 @@
+package Application;
+import java.util.List;
+import Model.Group;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GroupRepository extends CrudRepository<Group, Long> {
+    List<Group> findByName(String name);
+}
